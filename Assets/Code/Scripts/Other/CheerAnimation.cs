aee1c3939c1acd60d58097345d7fa8c498157db4
Celeste <c.sabater2001@gmail.com>
2025-05-03 20:09:35 +0200

    animación people

--- Assets/Code/Scripts/Other/CheerAnimation.cs
+++ Assets/Code/Scripts/Other/CheerAnimation.cs
@@ -10,9 +10,9 @@ public class CheerAnimation : MonoBehaviour
     void Start()
     {
         animator = GetComponent<Animator>();
-        GameEvents.current.onLowCombo += () => animator.Play("LowCombo");
-        GameEvents.current.onMidCombo += () => animator.Play("MidCombo");
-        GameEvents.current.onHighCombo += () => animator.Play("HighCombo");
+        GameEvents.current.onLowCombo += () => animator.Play("LowCombo"); animator.SetBool("LowCombo", true); animator.SetBool("MidCombo", false); animator.SetBool("HighCombo", false);
+        GameEvents.current.onMidCombo += () => animator.Play("MidCombo"); animator.SetBool("LowCombo", false); animator.SetBool("MidCombo", true); animator.SetBool("HighCombo", false);
+        GameEvents.current.onHighCombo += () => animator.Play("HighCombo"); animator.SetBool("LowCombo", false); animator.SetBool("MidCombo", false); animator.SetBool("HighCombo", true);
     }
 
     void OnDestroy()
